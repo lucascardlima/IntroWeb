7d85dd9c20a6984439ec8a1b3bb1af25d70ec65c
lucascardlima <142846329+lucascardlima@users.noreply.github.com>
2024-08-29 19:19:29 -0300

    atividade 2

--- projeto02/script.js
+++ projeto02/script.js
@@ -1,12 +1,11 @@
 
-<script>
     document.addEventListener('DOMContentLoaded', function () {
         var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
         var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
             return new bootstrap.Tooltip(tooltipTriggerEl);
         });
     });
-</script>
+
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
