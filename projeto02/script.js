7a38bbd8b2fa99786a7f4f5a7d0571ac472239b5
lucascardlima <142846329+lucascardlima@users.noreply.github.com>
2024-08-29 20:21:46 -0300

    alterações pra portugues

--- projeto02/script.js
+++ projeto02/script.js
@@ -10,3 +10,9 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+});
+
